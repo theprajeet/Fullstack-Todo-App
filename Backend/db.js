97aac38f13f97a5d142e3b0f2656e1196585186e
Prajeet <prajeethiremath@gmail.com>
2024-01-14 22:00:42 +0530

    backend added

---
+++ Backend/db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to the database');
+});
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todo = mongoose.model('todos', todoSchema);
+
+module.exports = {
+    todo
+}
---
+++ Backend/types.js
@@ -0,0 +1,16 @@
+const zod = require("zod");
+
+const creatTodo = zod.object({
+    title: zod.string(),
+    description: zod.string()
+})
+
+const updateTodo = zod.object({
+    id: zod.string(),
+})
+
+module.exports = {
+    creatTodo: creatTodo,
+    updateTodo: updateTodo
+}
+
